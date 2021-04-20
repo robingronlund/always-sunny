246fc01d2a6470287328704013d268b862b1dabd
Robin Grönlund <robin.gronlund@gmail.com>
2021-04-20 17:02:33 +0200

    Fetch Historical data

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Header from './Header'
-import '../styles/App.scss';
-import ForeCast from './Forecast';
+import Header from "./Header";
+import "../styles/App.scss";
+import ForeCast from "./Forecast";
+import EmptyState from "./EmptyState";
 
 function App() {
-  const [foreCast, setForeCast] = useState()
-  const [error, setError] = useState()
-  const cityName = 'Gothenburg'
+  const [state, setState] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(`https://api.openweathermap.org/data/2.5/forecast/hourly?q=${cityName}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
-  //     .then((resp) => resp.json())
-  //     .then((data) => {
-  //       let weather = data
-  //       console.log(weather)
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //       })
-  // }, [])
+  useEffect(() => {
+    fetchHistoricalData();
+  }, []);
 
+  const fetchHistoricalData = () => {
+    let history = 4;
+    for (let i = 0; i < 4; i++) {
+      let date = new Date(Date.now() - history * 24 * 3600 * 1000).getTime();
+
+      const unixTime = Math.floor(date / 1000.0);
+      const url = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=57.7&lon=11.9&dt=${unixTime}&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+          setState((state) => [...state, data.current]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      history--;
+    }
+  };
   return (
     <div className="page-container">
       <Header></Header>
-      <label>Calculated forecast from 4 days of weather data</label>
-      <ForeCast></ForeCast>
+      <label>Calculated temperature from 4 days of weather data</label>
+      {state.length > 0 ? (
+        <ForeCast weather={state}></ForeCast>
+      ) : (
+        <EmptyState></EmptyState>
+      )}
     </div>
   );
 }
