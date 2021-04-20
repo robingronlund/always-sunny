3bfa804aea2be28ddd5216b82f4584d5b305f87d
Robin Grönlund <robin.gronlund@gmail.com>
2021-04-20 17:02:53 +0200

    Add a simple empty state to handle empty data

---
+++ src/components/EmptyState.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Forecast.scss";
+
+const ForeCast = () => {
+  return (
+    <div className="forecast-wrapper">
+      <div className="forecast-min-max">
+        <p>No weather data available</p>
+      </div>
+    </div>
+  );
+};
+
+export default ForeCast;
