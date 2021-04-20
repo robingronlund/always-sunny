0e67e47713da60d880ac6f117999876d1ca2de0a
Robin Grönlund <robin.gronlund@gmail.com>
2021-04-20 17:03:23 +0200

    Add calculation methods to display temperature

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,64 +1,47 @@
-import React, { useState } from 'react'
-import '../styles/Forecast.scss'
+import React from "react";
+import "../styles/Forecast.scss";
 
-const initialState = {
-  minTemp: 0,
-  maxTemp: 0,
-  medianTemp: 0,
-  mediumTemp: 0,
-}
+const ForeCast = (props) => {
+  const { weather } = props;
 
-const forecast = [
-  {
-    temp: 20
-  },
-  {
-    temp: 16
-  },
-  {
-    temp: 12
-  },
-  {
-    temp: 23
-  },
-]
+  const temperatures = weather.map((w) => parseInt(w.temp, 10));
+  const sortedArray = temperatures
+    .map((temperature) => temperature)
+    .sort((a, b) => a - b);
 
-const ForeCast = () => {
-  const [state, setState] = useState(initialState)
-  const minTemp = forecast.reduce((acc, curr) => {
-    return (curr.temp < acc.temp ? curr : acc);
-  })
+  const minTemp = temperatures.reduce((acc, curr) => {
+    return curr < acc ? curr : acc;
+  });
 
-  const maxTemp = forecast.reduce((acc, curr) => {
-    return (curr.temp > acc.temp ? curr : acc);
-  })
+  const maxTemp = temperatures.reduce((acc, curr) => {
+    return curr > acc ? curr : acc;
+  });
 
   const avarage = () => {
-    const arr = forecast.map((forecast) => forecast.temp).sort()
-    const sum = arr.reduce((acc, curr) => curr += acc)
-    return sum / arr.length
-  }
+    const sum = sortedArray.reduce((acc, curr) => (curr += acc));
+    const avarage = sum / sortedArray.length;
+    return avarage;
+  };
 
   const median = () => {
-    const arr = forecast.map((forecast) => forecast.temp).sort((a, b) => a - b)
-    const highMiddle = Math.floor((arr.length -1) / 2)
-    const lowMiddle = Math.ceil((arr.length -1) / 2)
-    return (arr[lowMiddle] + arr[highMiddle]) / 2
-  }
+    const highMiddle = Math.floor((sortedArray.length - 1) / 2);
+    const lowMiddle = Math.ceil((sortedArray.length - 1) / 2);
+    const median = (sortedArray[lowMiddle] + sortedArray[highMiddle]) / 2;
+    return median;
+  };
 
   return (
     <div className="forecast-wrapper">
       <div className="forecast-min-max">
-        <p>Max - {maxTemp.temp}&#176;</p>
-        <p>Min - {minTemp.temp}&#176;</p>
+        <p>Max - {maxTemp}&#176;</p>
+        <p>Min - {minTemp}&#176;</p>
       </div>
       <div className="forecast-medium-median">
         <p>Avarage - {avarage()}&#176;</p>
         <p>Median - {median()}&#176;</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default ForeCast
+export default ForeCast;
