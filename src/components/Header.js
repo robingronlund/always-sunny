d883fe8bdf396bd600b79d77c1b37659d875e223
Robin Grönlund <robin.gronlund@gmail.com>
2021-04-20 17:03:58 +0200

    Cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import '../styles/Header.scss'
+import "../styles/Header.scss";
 
 const Header = () => {
-
   return (
     <header className="header-main">
       <div className="header-title">
-        <p>It's always sunny in
-          <span class="material-icons">
-            wb_sunny
-          </span>
+        <p>
+          It's always sunny in
+          <span className="material-icons">wb_sunny</span>
         </p>
         <h1>Gothenburg</h1>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
